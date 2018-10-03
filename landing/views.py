b5f5dbf92a1d5cf3a20def34c42fa4f9c94d84d7
JackShort <john.short.tj@gmail.com>
2018-10-03 10:59:13 -0400

    added sign up authentication

--- landing/views.py
+++ landing/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.views.generic import TemplateView
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
 
 class HomePageView(TemplateView):
     template_name = "index.html"
-
 
 class AboutPageView(TemplateView):
     template_name = "about.html"
@@ -17,3 +19,23 @@ class LoginPageView(TemplateView):
 
 class SignupPageView(TemplateView):
     template_name = "signup.html"
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            rawPassword = form.cleaned_data.get('password')
+            user = authenticate (username=username, password=rawPassword)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'signup.html', {'form': form})
+
+def login(request):
+    username = request.POST('username')
+    password = request.POST('password')
+    user = authenticate(request, username=username, password=password)
