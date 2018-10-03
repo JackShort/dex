8b1675f44d0df1873e160192234a6f8bb626a7c3
JackShort <john.short.tj@gmail.com>
2018-10-03 11:04:48 -0400

    working on login

--- landing/views.py
+++ landing/views.py
@@ -35,7 +35,11 @@ def signup(request):
 
     return render(request, 'signup.html', {'form': form})
 
-def login(request):
-    username = request.POST('username')
-    password = request.POST('password')
-    user = authenticate(request, username=username, password=password)
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST('username')
+        password = request.POST('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
