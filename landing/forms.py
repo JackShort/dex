c3d10b14b5f352e661608fefeee93add5c52b1ee
Joe Karaki <jfk2bd@virginia.edu>
2018-10-15 23:06:04 -0400

    FormValidation
    form validation

--- landing/forms.py
+++ landing/forms.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django import forms
+from django.db import models
 from .models import Profile
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class UserForm(forms.ModelForm):
     class Meta:
@@ -16,3 +18,17 @@ class ProfileForm(forms.ModelForm):
         if zipcode != '5':
             raise forms.ValidationError("fuck you joe")
         return zipcode
+    def clean_address_1(self):
+       	address_1 = self.cleaned_data['address_1']
+       	address_1 = address_1.replace(" ", "")
+        if not address_1.isalnum():
+        	raise forms.ValidationError("Please use only Alphanumeric characters in the Address box")
+        return address_1
+    def clean_city(self):
+       	city = self.cleaned_data['city']
+       	city = city.replace(" ", "")
+        if not city.isalpha():
+        	raise forms.ValidationError("Please use only letters in the City box")
+        return city
+								
+
--- landing/models.py
+++ landing/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 US_STATES = (
     ('AL', 'Alabama'),
@@ -64,7 +65,8 @@ class Profile(models.Model):
 	address_2 = models.CharField(max_length=100, default='')
 	city = models.CharField(max_length=50)
 	state = models.CharField(max_length=2, choices=US_STATES)
-	zipcode = models.PositiveIntegerField(null=True)
+	zipcode = models.PositiveIntegerField(validators=[MinValueValidator(0),
+        MaxValueValidator(99999)])
 
 	# @receiver(post_save, sender=User)
 	# def create_user_profile(sender, instance, created, **kwargs):
