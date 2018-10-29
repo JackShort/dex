6381103d5a32914effa64419712abdba8b3bd141
Jack Schefer <jack.schefer9@gmail.com>
2018-10-28 22:04:21 -0400

    send confirmation email on user creation

--- r3tr0/settings.py
+++ r3tr0/settings.py
@@ -28,6 +28,14 @@ DEBUG = True
 ALLOWED_HOSTS = ['thedex.herokuapp.com', 'localhost', '127.0.0.1']
 
 
+# Email stuff
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'dex.app.service@gmail.com'
+EMAIL_HOST_PASSWORD = 'dexappservice' # super security
+
 # Application definition
 
 INSTALLED_APPS = [
